30801973a30a83a3a09fa7715379a058be8e3468
ZEnglish89 <82051602+ZEnglish89@users.noreply.github.com>
2021-04-08 09:51:05 -0700

    Initial commit

---
+++ Student.js
@@ -0,0 +1,11 @@
+
+
+
+let Student = function(id,name,age,grade) {
+	this.id = id;
+	this.name = name;
+	this.age = age; //Number input(no stated min/max)
+	this.grade = grade; //9,10,11,12 Dropdown
+}
+
+module.exports = Student;
